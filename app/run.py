521bcfa67ae9d398c1759ea23e0262a7742a197d
Gabriel Dias de Abreu <gabriel.abreu@softplan.com.br>
2021-01-11 21:37:17 -0300

    feature: flask web server for message classification and train data overview

---
+++ app/run.py
@@ -0,0 +1,127 @@
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+import joblib
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+def tokenize(text):
+    """
+        receives a String text from disaster pipeline and returns lemmatized, normatized tokens
+    """
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('data/DisasterResponse.db',engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    categories = ['related', 'request', 'offer',
+           'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
+           'security', 'military', 'child_alone', 'water', 'food', 'shelter',
+           'clothing', 'money', 'missing_people', 'refugees', 'death', 'other_aid',
+           'infrastructure_related', 'transport', 'buildings', 'electricity',
+           'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
+           'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+           'other_weather', 'direct_report']
+    df_sum = df[categories].sum()
+    # create visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=df_sum.index,
+                    y=df_sum
+                )
+            ],
+
+            'layout': {
+                'title': 'Amount of samples by each category',
+                'yaxis': {
+                    'title': "Amount (un)"
+                },
+                'xaxis': {
+                    'title': "Categoryd;"
+                }
+            }
+        }
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
